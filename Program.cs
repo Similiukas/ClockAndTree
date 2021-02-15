e5e75c447b01ece4247de5ce368f538e648ab760
Simonas Tautvaišas <tautvaisassimas@gmail.com>
2021-02-15 12:24:26 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClockAndTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!\n");
+        }
+    }
+}
